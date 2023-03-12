7f057cfb82e12f5c9a21b0c5f35cde5cce914872
Himanshu <himanshu.2231999@gmail.com>
2023-03-12 16:07:42 +0530

    Adding more programs

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,16 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+
+		int j=20;
+		do {
+			
+			System.out.println(j);
+			j++;
+		
+		}while(j<30);
+
+	}
+
+}
---
+++ src/InvertedPyramidTriangle.java
@@ -0,0 +1,38 @@
+
+public class InvertedPyramidTriangle {
+
+	public static void main(String[] args) {
+		
+		int k=1;
+		for(int i=1;i<=4;i++) {
+			
+			for(int j=0;j<i;j++) {
+				System.out.print(k+" ");
+				k++;
+			}
+			
+			System.out.println();
+		}
+		
+//		for(int i=1;i<5;i++) {
+//			
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(j+" ");
+//			}
+//			System.out.println();
+//		}
+		
+//		int k=3;
+//		for(int i=1;i<=3;i++) {
+//			
+//			for(int j=0;j<i;j++) {
+//				System.out.print(k+" ");
+//				k+=3;
+//			}
+//			
+//			System.out.println();
+//		}
+//		
+	}
+
+}
